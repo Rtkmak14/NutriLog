805b231f52a1dc433f31d0fc18d32e887d2bf62a
Rtkmak14 <Rtkmak14@gmail.com>
2025-09-09 15:27:51 -0500

    finalization of foods controller and edits.ejs page

--- controllers/food.js
+++ controllers/food.js
@@ -79,4 +79,19 @@ router.get(`/:foodId/edit`, async (req,res)=> {
     }
 })
 
+router.put(`/:foodId`, async (req,res)=>{
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        const food = currentUser.foods.id(req.params.foodId)
+        food.set(req.body)
+        await currentUser.save()
+        res.redirect(`/users/${currentUser._id}/foods/${req.params.foodId}`)
+    }
+
+    catch (error) {
+        console.log(error)
+        res.redirect(`/`)
+    }
+})
+
 module.exports = router
