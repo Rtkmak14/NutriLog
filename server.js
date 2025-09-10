76e47af1c4e5dc0bd91d566cc58967b3a9e04fc0
Rtkmak14 <Rtkmak14@gmail.com>
2025-09-10 13:21:38 -0500

    basline css file linkage and update to move all nav elements above h1 elements

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const foodsController = require("./controllers/food.js")
 const usersController = require("./controllers/users.js")
 
 const port = process.env.PORT? process.env.PORT: "3000"
+const path = require('path')
 
 
 const methodOverride = require("method-override")
@@ -32,6 +33,7 @@ const passUserToView = require("./middleware/pass-user-to-view.js")
 
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride(`_method`))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
